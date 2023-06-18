ab84b21e5de8c21f91b8f1d88a37060e9c9eee5a
Hugo <hugosantosadm125@gmail.com>
2023-06-17 22:36:21 -0300

    Criando a classe PageAdmin e Tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+//Namespace:
+namespace Hcode;
+
+
+class PageAdmin extends Page {
+
+	//Atributos:
+
+
+	//Metodos:
+	 public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+	 {
+
+	 	parent::__construct($opts, $tpl_dir);
+
+	 }
+
+}
+
+
+
+?>
