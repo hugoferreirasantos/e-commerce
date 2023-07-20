2cd25f65a930be69e4f23cb949da699cc1dd290f
Hugo <hugosantosadm125@gmail.com>
2023-07-19 22:02:04 -0300

    Carrinho Produtos

--- site.php
+++ site.php
@@ -76,7 +76,65 @@ $app->get('/cart',function(){
 
     $page = new Page();
 
-    $page->setTpl("cart");
+    $page->setTpl("cart",[
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts()
+    ]);
+
+});
+
+//Rota para adicionar produtos no carrinho:
+$app->get('/cart/:idproduct/add',function($idproduct){
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $cart = Cart::getFromSession();
+
+    $qtd = (isset($_GET["qtd"])) ? (int)$_GET["qtd"] : 1;
+
+    for($i = 0; $i < $qtd ; $i++ ){
+
+        $cart->addProduct($product);
+
+    }
+
+
+    header("Location: /cart");
+    exit;
+
+});
+
+//Rota para remover um produto do carrinho:
+$app->get('/cart/:idproduct/minus',function($idproduct){
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $cart = Cart::getFromSession();
+
+    $cart->removeProduct($product);
+
+    header("Location: /cart");
+    exit;
+
+});
+
+//Rota para remover todos os produtos do carrinho:
+$app->get('/cart/:idproduct/remove',function($idproduct){
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $cart = Cart::getFromSession();
+
+    $cart->removeProduct($product,true);
+
+    header("Location: /cart");
+    exit;
 
 });
 
